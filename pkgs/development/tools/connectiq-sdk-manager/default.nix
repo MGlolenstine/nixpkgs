e1f4b2e4e25e072e759d95b18160fb7fee7d3a3f
MGlolenstine <mglolenstine@gmail.com>
2023-02-13 07:15:30 +0100

    connectiq-sdk-manager: Init at unversioned.

--- pkgs/development/tools/connectiq-sdk-manager/default.nix
+++ pkgs/development/tools/connectiq-sdk-manager/default.nix
@@ -1,90 +1,59 @@
 { stdenv
+, pkgs
 , lib
-, fetchurl
+, fetchzip
 , unzip
-, zlib
-, curl
-, libsecret
-, gcc
-, expat
-, glib
-, xorg_sys_opengl
-, gtk3-x11
-, xorg
-, atk
-, freetype
-, libpng
-, cairo
-, gnome2
-, gdk-pixbuf
-, webkitgtk
-, fontconfig
-, libjpeg8
-, glib-networking
+, buildFHSUserEnv
+, autoPatchelfHook
+, wrapGAppsHook
 }:
-stdenv.mkDerivation rec {
+let 
+  pkgIncludes = with pkgs; [
+    zlib
+    curl
+    libsecret
+    gcc
+    expat
+    glib
+    xorg_sys_opengl
+    gtk3-x11
+    xorg.libX11
+    xorg.libXxf86vm
+    xorg.libSM
+    atk
+    freetype
+    libpng
+    cairo
+    gnome2.pango
+    gdk-pixbuf
+    webkitgtk
+    gnome2.libsoup
+    fontconfig
+    libjpeg8
+    glib-networking
+    stdenv.cc.cc.lib
+  ];
+in
+stdenv.mkDerivation {
   name = "connectiq-sdk-manager";
 
-  src = fetchurl {
+  src = fetchzip {
     url = "https://developer.garmin.com/downloads/connect-iq/sdk-manager/connectiq-sdk-manager-linux.zip";
-    sha256 = "8e51e167011965c44ed108be021cfbca9e89bc03ce1d4e29edde9b2c257e1c9a";
+    sha256 = "sha256-g43pyQCYC12nkPrRdMz/K036X78n/NVrN/4OlVP8qkw=";
+    stripRoot = false;
   };
-  sourceRoot = ".";
 
-  dontConfigure = true;
-  dontBuild = true;
-  
-  nativeBuildInputs = [unzip];
-
-  unpackPhase = ''
-    unzip ${src}
-  '';
+  nativeBuildInputs = [ autoPatchelfHook wrapGAppsHook ] ++ pkgIncludes;
 
   installPhase = ''
-    mkdir -p $out/opt
-    mkdir -p $out/bin
-    cp -r bin $out/opt
-    cp -r share $out/opt
-    ln -s $out/opt/bin/sdkmanager $out/bin/sdkmanager
-  '';
-
-  preFixup = let
-    libPath = lib.makeLibraryPath [
-      zlib
-      curl
-      libsecret
-      gcc
-      expat
-      glib
-      xorg_sys_opengl
-      gtk3-x11
-      xorg.libX11
-      xorg.libXxf86vm
-      xorg.libSM
-      atk
-      freetype
-      libpng
-      cairo
-      gnome2.pango
-      gdk-pixbuf
-      webkitgtk
-      gnome2.libsoup
-      fontconfig
-      libjpeg8
-      glib-networking
-      stdenv.cc.cc.lib
-    ];
-  in ''
-    patchelf \
-      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${libPath}" \
-      $out/bin/sdkmanager
+    mkdir -p $out
+    cp -r $src/* $out
   '';
 
   meta = with lib; {
     homepage = https://developer.garmin.com/connect-iq/sdk/; 
     license = licenses.unfree;
     platforms = platforms.linux;
-    maintainers = [ ];
+    maintainers = [ maintainers.mglolenstine ];
   };
 }
