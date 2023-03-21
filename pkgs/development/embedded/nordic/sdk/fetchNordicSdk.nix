6aa0e0ac54164d685849a2143ad710bcafde95e4
MGlolenstine <mglolenstine@gmail.com>
2023-03-21 20:58:12 +0100

    Added cmake and started patching configuration files.

--- pkgs/development/embedded/nordic/sdk/fetchNordicSdk.nix
+++ pkgs/development/embedded/nordic/sdk/fetchNordicSdk.nix
@@ -31,15 +31,27 @@ stdenv.mkDerivation {
 
   propagatedBuildInputs = with pkgs; [
     python3.pkgs.west
+    cmake
   ];
 
   buildPhase = ''
     mkdir -p $out/sdk
+    mkdir -p $out/lib/cmake
 
     cp $src $out/sdk/west.yml
     west -v init -l $out/sdk/
     cd $out/sdk
     west -v update
+
+    # Redirect to the package and not $HOME
+    echo "sed -i \"s,\$ENV{HOME}/.cmake,$out/lib/cmake,g\" $out/zephyr/share/zephyr-package/cmake/zephyr_export.cmake"
+    echo "sed -i \"s,~/.cmake,$out/lib/cmake,g\" $out/zephyr/share/zephyr-package/cmake/zephyr_export.cmake"
+    echo sed -i 's,\$\{CMAKE_CURRENT_LIST_DIR\}/\$\{MD5_INFILE\},'$out'/tmp/\$\{MD5_INFILE\},g' $out/zephyr/share/zephyr-package/cmake/zephyr_export.cmake
+    
+    sed -i "s,\$ENV{HOME}/.cmake,$out/lib/cmake,g" $out/zephyr/share/zephyr-package/cmake/zephyr_export.cmake
+    sed -i "s,~/.cmake,$out/lib/cmake,g" $out/zephyr/share/zephyr-package/cmake/zephyr_export.cmake
+    sed -i 's,\$\{CMAKE_CURRENT_LIST_DIR\}/\$\{MD5_INFILE\},'$out'/tmp/\$\{MD5_INFILE\},g' $out/zephyr/share/zephyr-package/cmake/zephyr_export.cmake
+    
     west -v zephyr-export
   '';
 }
